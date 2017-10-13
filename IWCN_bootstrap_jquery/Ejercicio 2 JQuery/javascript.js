41e82ab8ede820b516c26cca1a717f65dd5aeef6
José Antonio Morante Sánchez <ruben.morante.gonzalez@outlook.com>
2017-10-13 23:54:01 +0200

    subir todo

---
+++ IWCN_bootstrap_jquery/Ejercicio 2 JQuery/javascript.js
@@ -0,0 +1,24 @@
+function myFunction() {
+	var popup = document.getElementById("popup"),
+	fname = document.getElementById("fname").value, 
+	lname = document.getElementById("lname").value,
+	phone = document.getElementById("phone").value,
+	email = document.getElementById("email").value,
+	message = document.getElementById("message").value;
+
+	document.getElementById("fnamePopup").innerText=fname;
+	document.getElementById("lnamePopup").innerText=lname;
+	document.getElementById("phonePopup").innerText=phone;
+	document.getElementById("emailPopup").innerText=email;
+	document.getElementById("messagePopup").innerText=message;
+
+	if(fname && lname && email && message){
+		popup.className='visible';
+	}
+}
+
+
+function cerrar(){
+
+	$('#popup').removeClass('visible');
+}
