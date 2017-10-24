fa4ae446cd4d5c37a197bf1cb5f0ca45cd8dc93a
José Antonio Morante Sánchez <ruben.morante.gonzalez@outlook.com>
2017-10-24 12:03:01 +0200

    security sencillo

---
+++ IWCN_spring_security/ejercicio3.4/src/main/java/main/controllers/ClassController.java
@@ -0,0 +1,78 @@
+package main.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import main.model.Product;
+
+import main.services.ProductServiceDB;
+
+@Controller
+public class ClassController {
+	
+	@Autowired
+	private ProductServiceDB productService;
+	
+	@RequestMapping("/")
+    public String welcome() {
+        return "welcome";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+	
+	@RequestMapping("/home")
+	public String index() {			
+		return "home";
+	}
+	
+	@RequestMapping("/addProduct")
+	public String addProduct() {			
+		return "addProduct";
+	}
+	
+	@RequestMapping("/add")
+	public String add(Product product) {
+		this.productService.add(product);
+		return "home";
+	}
+	
+	@RequestMapping("/list")
+	public String list(Model model) {
+		model.addAttribute("products", this.productService.findAll());
+		return "list";
+	}
+	
+	@RequestMapping("/show")
+	public String show(@RequestParam int numProduct, Model model) {
+		Product product = this.productService.get(numProduct);
+		model.addAttribute("product", product);
+		return "show";
+	}
+	
+	@RequestMapping("/delete")
+	public String delete(@RequestParam int numProduct, Model model) {
+		this.productService.remove(numProduct);
+		model.addAttribute("products", this.productService.findAll());
+		return "list";
+	}
+	
+	@RequestMapping("/editProduct")
+	public String editProduct (@RequestParam int numProduct, Model model) {
+		Product product = this.productService.get(numProduct);
+		model.addAttribute("product", product);
+		return "editProduct";
+	}
+
+	@RequestMapping("/edit")
+	public String edit(Product product, Model model) {
+		this.productService.add(product);
+		model.addAttribute("products", this.productService.findAll());
+		return "list";
+	}
+}
