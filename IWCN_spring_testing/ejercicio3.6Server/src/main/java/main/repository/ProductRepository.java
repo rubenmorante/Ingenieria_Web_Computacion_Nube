7bb3e4362fa3c7790df1524a513765dcc4434625
José Antonio Morante Sánchez <ruben.morante.gonzalez@outlook.com>
2017-11-13 20:41:58 +0100

    .

--- IWCN_spring_testing/ejercicio3.6Server/src/main/java/main/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package main.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import main.model.Product;
-
-public interface ProductRepository extends CrudRepository<Product, Integer> {
-
-}
